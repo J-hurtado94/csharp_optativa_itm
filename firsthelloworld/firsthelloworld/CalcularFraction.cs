a5da07a2bcaad412bfe3cbfb29e2418dc1b5c57f
J-hurtado94 <juanhurtado2994@gmail.com>
2023-09-09 18:29:11 -0500

    Set project

--- firsthelloworld/firsthelloworld/CalcularFraction.cs
+++ firsthelloworld/firsthelloworld/CalcularFraction.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using firsthelloworld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ejerciciosoptativa
+namespace firsthelloworld
 {
     //Descripción: Solicita al usuario los valores para dos fracciones y muestra la diferencia entre esas fracciones.
     internal class CalcularFraction
--- firsthelloworld/firsthelloworld/Main.cs
+++ firsthelloworld/firsthelloworld/Main.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using firsthelloworld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ejerciciosoptativa
+namespace firsthelloworld
 {
     class Ejecucion
     {
